e00ef96a806dbca0fe69a8675ab6f68598022a84
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 17:22:03 +0100

    Creates URLSession data task with URL on `URLSessionHTTPClientForArticles.get(from: URL)`

---
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
@@ -0,0 +1,85 @@
+//
+// URLSessionHTTPClientForArticlesTests.swift
+// Created by Mikayil M on 20/06/2021
+// 
+//
+
+import XCTest
+import DailyFXFetchEngine
+
+final class URLSessionHTTPClientForArticles: HTTPClientForArticles {
+    
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        let wrapped: URLSessionTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClientForArticles.Result) -> Void) -> HTTPClientTask {
+        let task = session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
+    }
+}
+
+
+class URLSessionHTTPClientForArticlesTests: XCTestCase {
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        URLProtocolStub.removeStub()
+    }
+    
+    func test_getFromURL_performsGETRequestWithURL() {
+        let url = anyURL()
+        let exp = expectation(description: "Wait for request")
+        
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        makeSUT().get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClientForArticles {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        
+        let sut = URLSessionHTTPClientForArticles(session: session)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
