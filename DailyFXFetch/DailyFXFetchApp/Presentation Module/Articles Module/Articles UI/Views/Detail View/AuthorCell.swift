95506f76309073d40c482359f96993670ecf3c21
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 20:48:09 +0100

    Link AuthorCell to the tableview in Author Detail View.

---
+++ DailyFXFetch/DailyFXFetchApp/Presentation Module/Articles Module/Articles UI/Views/Detail View/AuthorCell.swift
@@ -0,0 +1,16 @@
+//
+// AuthorCell.swift
+// Created by Mikayil M on 20/06/2021
+// Copyright © 2021 Mikayil M. All rights reserved.
+//
+
+import UIKit
+
+final class AuthorCell: UITableViewCell {
+    
+    @IBOutlet private(set) public var name: UILabel!
+    @IBOutlet private(set) public var title: UILabel!
+    @IBOutlet private(set) public var descriptionShort: UILabel!
+    @IBOutlet private(set) public var photo: UIImageView!
+
+}
