2aab0d309dbd6bc0e47c7e347e14da3f08532332
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 18:29:03 +0100

    Cancel LoadImageDataURLTask cancels URLRequest on the client's side.

--- DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/LoadImageDataFromRemoteUseCaseTests.swift
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/LoadImageDataFromRemoteUseCaseTests.swift
@@ -125,6 +125,17 @@ class LoadImageDataFromRemoteUseCaseTests: XCTestCase {
         })
     }
     
+    func test_cancelLoadImageDataURLTask_cancelsClientURLRequest() {
+        let (sut, client) = makeSUT()
+        let url = URL(string: "https://a-given-url.com")!
+
+        let task = sut.loadImageData(from: url) { _ in }
+        XCTAssertTrue(client.cancelledURLs.isEmpty, "Expected no cancelled URL request until task is cancelled")
+        
+        task.cancel()
+        XCTAssertEqual(client.cancelledURLs, [url], "Expected cancelled URL request after task is cancelled")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteImageDataLoader, client: HTTPClientForArticlesSpy) {
