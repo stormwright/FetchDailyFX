570997537f31a415a7a6b2932d56d5f9811035af
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 17:26:16 +0100

    Cancels URLRequest when the get request from url is cancelled.

--- DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
@@ -66,6 +66,12 @@ class URLSessionHTTPClientForArticlesTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_cancelGetFromURLTask_cancelsURLRequest() {
+        let receivedError = resultErrorFor(taskHandler: { $0.cancel() }) as NSError?
+
+        XCTAssertEqual(receivedError?.code, URLError.cancelled.rawValue)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClientForArticles {
@@ -76,6 +82,34 @@ class URLSessionHTTPClientForArticlesTests: XCTestCase {
         let sut = URLSessionHTTPClientForArticles(session: session)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func resultErrorFor(_ values: (data: Data?, response: URLResponse?, error: Error?)? = nil, taskHandler: (HTTPClientTask) -> Void = { _ in }, file: StaticString = #file, line: UInt = #line) -> Error? {
+        let result = resultFor(values, taskHandler: taskHandler, file: file, line: line)
+        
+        switch result {
+        case let .failure(error):
+            return error
+        default:
+            XCTFail("Expected failure, got \(result) instead", file: file, line: line)
+            return nil
+        }
+    }
+    
+    private func resultFor(_ values: (data: Data?, response: URLResponse?, error: Error?)?, taskHandler: (HTTPClientTask) -> Void = { _ in },  file: StaticString = #file, line: UInt = #line) -> HTTPClientForArticles.Result {
+        values.map { URLProtocolStub.stub(data: $0, response: $1, error: $2) }
+        
+        let sut = makeSUT(file: file, line: line)
+        let exp = expectation(description: "Wait for completion")
+        
+        var receivedResult: HTTPClientForArticles.Result!
+        taskHandler(sut.get(from: anyURL()) { result in
+            receivedResult = result
+            exp.fulfill()
+        })
+        
+        wait(for: [exp], timeout: 1.0)
+        return receivedResult
     }
     
 }
