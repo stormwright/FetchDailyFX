73455a7d79d0595ae82d0b05ad6c0948811174d9
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 17:39:33 +0100

    Move URLSessionHTTPClientForArticles to production.

---
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngine/Articles API/URLSessionHTTPClientForArticles.swift
@@ -0,0 +1,42 @@
+//
+// URLSessionHTTPClientForArticles.swift
+// Created by Mikayil M on 20/06/2021
+// 
+//
+
+import Foundation
+
+public final class URLSessionHTTPClientForArticles: HTTPClientForArticles {
+    
+    private let session: URLSession
+    
+    public init(session: URLSession) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        let wrapped: URLSessionTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientForArticles.Result) -> Void) -> HTTPClientTask {
+        let task = session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
+    }
+}
--- DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
@@ -7,42 +7,6 @@
 import XCTest
 import DailyFXFetchEngine
 
-final class URLSessionHTTPClientForArticles: HTTPClientForArticles {
-    
-    private let session: URLSession
-    
-    init(session: URLSession) {
-        self.session = session
-    }
-    
-    private struct UnexpectedValuesRepresentation: Error {}
-    
-    private struct URLSessionTaskWrapper: HTTPClientTask {
-        let wrapped: URLSessionTask
-        
-        func cancel() {
-            wrapped.cancel()
-        }
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPClientForArticles.Result) -> Void) -> HTTPClientTask {
-        let task = session.dataTask(with: url) { data, response, error in
-            completion(Result {
-                if let error = error {
-                    throw error
-                } else if let data = data, let response = response as? HTTPURLResponse {
-                    return (data, response)
-                } else {
-                    throw UnexpectedValuesRepresentation()
-                }
-            })
-        }
-        task.resume()
-        return URLSessionTaskWrapper(wrapped: task)
-    }
-}
-
-
 class URLSessionHTTPClientForArticlesTests: XCTestCase {
     
     override func tearDown() {
