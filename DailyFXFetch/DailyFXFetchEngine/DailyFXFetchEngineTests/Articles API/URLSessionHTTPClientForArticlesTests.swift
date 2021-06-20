6fcf2875378cf6d698ab73b59c6491b0ecd33732
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 17:32:22 +0100

    The get from URL request succeeds on HTTTPURLResponse with valid data.

--- DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
@@ -92,6 +92,17 @@ class URLSessionHTTPClientForArticlesTests: XCTestCase {
         XCTAssertNotNil(resultErrorFor((data: anyData(), response: nonHTTPURLResponse(), error: nil)))
     }
     
+    func test_getFromURL_succeedsOnHTTPURLResponseWithData() {
+        let data = anyData()
+        let response = anyHTTPURLResponse()
+        
+        let receivedValues = resultValuesFor((data: data, response: response, error: nil))
+        
+        XCTAssertEqual(receivedValues?.data, data)
+        XCTAssertEqual(receivedValues?.response.url, response.url)
+        XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClientForArticles {
@@ -130,6 +141,18 @@ class URLSessionHTTPClientForArticlesTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
         return receivedResult
+    }
+    
+    private func resultValuesFor(_ values: (data: Data?, response: URLResponse?, error: Error?), file: StaticString = #file, line: UInt = #line) -> (data: Data, response: HTTPURLResponse)? {
+        let result = resultFor(values, file: file, line: line)
+
+        switch result {
+        case let .success((data, response)):
+            return (data, response)
+        default:
+            XCTFail("Expected success, got \(result) instead", file: file, line: line)
+            return nil
+        }
     }
     
     private func anyHTTPURLResponse() -> HTTPURLResponse {
