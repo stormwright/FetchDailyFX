909a2ffda09c006037b790f3bc6583cfaf6ceb3e
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 23:06:42 +0100

    Add ArticleCell class to render ArticleCell view.

---
+++ DailyFXFetch/DailyFXFetchApp/Presentation Module/Articles Module/Articles UI/Views/Dashboard/ArticleCell.swift
@@ -0,0 +1,13 @@
+//
+// ArticleCell.swift
+// Created by Mikayil M on 20/06/2021
+// Copyright © 2021 Mikayil M. All rights reserved.
+//
+
+import UIKit
+
+final class ArticleCell: UITableViewCell {
+    @IBOutlet private(set) var title: UILabel!
+    @IBOutlet private(set) var date: UILabel!
+    @IBOutlet private(set) var articleImage: UIImageView!
+}
