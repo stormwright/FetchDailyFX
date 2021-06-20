16c73ce165a9406ed5a6b447caf3f40040cf27ae
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 17:36:06 +0100

    Extract shared tester methods to a separate file.

--- DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/URLSessionHTTPClientForArticlesTests.swift
@@ -175,15 +175,3 @@ class URLSessionHTTPClientForArticlesTests: XCTestCase {
     }
     
 }
-
-func anyURL() -> URL {
-    return URL(string: "http://any-url.com")!
-}
-
-func anyNSError() -> NSError {
-    return NSError(domain: "any error", code: 0)
-}
-
-func anyData() -> Data {
-    return Data("any data".utf8)
-}
---
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,19 @@
+//
+// SharedTestHelpers.swift
+// Created by Mikayil M on 20/06/2021
+// 
+//
+
+import Foundation
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyData() -> Data {
+    return Data("any data".utf8)
+}
