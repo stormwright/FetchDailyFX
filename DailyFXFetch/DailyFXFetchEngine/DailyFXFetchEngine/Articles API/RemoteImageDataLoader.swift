972aa7d64ef81e04bb8ceb0ad7e12a51a88b422b
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-20 18:34:00 +0100

    Move RemoteImageDataLoader to production.

---
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngine/Articles API/RemoteImageDataLoader.swift
@@ -0,0 +1,59 @@
+//
+// RemoteImageDataLoader.swift
+// Created by Mikayil M on 20/06/2021
+// 
+//
+
+import Foundation
+
+public final class RemoteImageDataLoader: ImageDataLoader {
+    
+    private let client: HTTPClientForArticles
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public init(client: HTTPClientForArticles) {
+        self.client = client
+    }
+    
+    private final class HTTPClientTaskWrapper: ImageDataLoaderTask {
+        private var completion: ((ImageDataLoader.Result) -> Void)?
+        
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (ImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: ImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+        
+        private func preventFurtherCompletions() {
+            completion = nil
+        }
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (ImageDataLoader.Result) -> Void) -> ImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap { (data, response) in
+                    let isValidResponse = response.isOK && !data.isEmpty
+                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                })
+        }
+        return task
+    }
+}
--- DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/LoadImageDataFromRemoteUseCaseTests.swift
+++ DailyFXFetch/DailyFXFetchEngine/DailyFXFetchEngineTests/Articles API/LoadImageDataFromRemoteUseCaseTests.swift
@@ -7,58 +7,6 @@
 import XCTest
 import DailyFXFetchEngine
 
-final class RemoteImageDataLoader: ImageDataLoader {
-    
-    private let client: HTTPClientForArticles
-    
-    enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    init(client: HTTPClientForArticles) {
-        self.client = client
-    }
-    
-    private final class HTTPClientTaskWrapper: ImageDataLoaderTask {
-        private var completion: ((ImageDataLoader.Result) -> Void)?
-        
-        var wrapped: HTTPClientTask?
-        
-        init(_ completion: @escaping (ImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: ImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-    
-    func loadImageData(from url: URL, completion: @escaping (ImageDataLoader.Result) -> Void) -> ImageDataLoaderTask {
-        let task = HTTPClientTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            task.complete(with: result
-                .mapError { _ in Error.connectivity }
-                .flatMap { (data, response) in
-                    let isValidResponse = response.isOK && !data.isEmpty
-                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
-                })
-        }
-        return task
-    }
-}
-
 class LoadImageDataFromRemoteUseCaseTests: XCTestCase {
     
     func test_init_doesNotPerformAnyURLRequest() {
