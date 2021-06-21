daf82658940954cae3fad8a224385688d8a7eefe
Mikayil Mammadov <mikayil.mammadov@proagrica.com>
2021-06-21 08:37:25 +0100

    Add Markets storyboard.

---
+++ DailyFXFetch/DailyFXFetchApp/DailyFXFetchApp/Presentation Module/Markets Module/Markets UI/Views/MarketCell.swift
@@ -0,0 +1,12 @@
+//
+// MarketCell.swift
+// Created by Mikayil M on 21/06/2021
+// Copyright © 2021 Mikayil M. All rights reserved.
+//
+
+import UIKit
+
+final class MarketCell: UITableViewCell {
+    @IBOutlet private(set) var marketName: UILabel!
+    @IBOutlet private(set) var marketID: UILabel!
+}
